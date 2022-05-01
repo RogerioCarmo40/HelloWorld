5b98b64d5b36eb575504c4906ba796da9517446d
Rogério Carmo <rogeriocarmo.ti@gmail.com>
2022-05-01 16:14:05 -0400

    Segundo Commit
    Mais um Hello World galera!!!!

---
+++ Hello.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package helloworld;
+
+/**
+ * @author Rogerio
+ * Hello World
+ */
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World of Java language! I am Rogerio programming student!");
+
+	}
+
+}
