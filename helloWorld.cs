766ed7a82b4067f8d0cad5699843a39ea486706f
Rogério Carmo <rogeriocarmo.ti@gmail.com>
2023-06-30 12:19:44 -0400

    Hello World in CSharp ou C#
    He writes hello world on his machine screen!

---
+++ helloWorld.cs
@@ -0,0 +1,12 @@
+// Online C# Editor
+// Write, Edit and Run your C# code using C# Online Compiler
+
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine ("Hello Mono World");
+    }
+}
